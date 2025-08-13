127669f996c5a69fc465d8fc007e7084d45e3b42
evrwaqe <evrwaqe@gmail.com>
2025-08-12 23:59:57 -0300

    refactor: reset project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import GitHubLoginButton from '@/components/GitHubLoginButton'
-
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
-        <h1 className="text-2xl font-bold text-center mb-6">
-          Next.js Firebase Auth
-        </h1>
-        <div className="flex justify-center">
-          <GitHubLoginButton />
-        </div>
-      </div>
+    <div>
+      <h1>GH Project Creator</h1>
     </div>
   )
 }
